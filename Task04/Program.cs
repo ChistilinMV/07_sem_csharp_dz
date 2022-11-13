83c1f4291f940242cfa39ff111f38611db36b1a0
MegaLitr <max.chistilin@gmail.com>
2022-11-13 22:42:34 +0300

    done task 4

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -3,4 +3,13 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число А: ");
+int num01 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int num02 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число С: ");
+int num03 = Convert.ToInt32(Console.ReadLine());
+int max=num01;
+if (num02>max) {max=num02;}
+if (num03>max) {max=num03;}
+Console.Write($"Максимальное число: {max}");
