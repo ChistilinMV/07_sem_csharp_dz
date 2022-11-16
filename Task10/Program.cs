fcc679820f843c01266ed8a7d708d1ccb6d38795
MegaLitr <max.chistilin@gmail.com>
2022-11-16 15:35:37 +0300

    done Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру
+// этого числа.
+
+string istr = "";
+Console.WriteLine("Введите 3-х значное число");
+while (istr.Length < 3 | istr.Length > 3 )
+{
+ istr = Console.ReadLine();
+}
+Console.Write("Вторая цифра введённого числа: "); Console.WriteLine(istr[1]);
+
