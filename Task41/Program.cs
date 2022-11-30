aa6c18c2494206179d98c14eb76afd859928ad74
MegaLitr <max.chistilin@gmail.com>
2022-11-30 23:01:39 +0300

    done Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите длину массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] sArr = new int[M];
+int count = 0;
+for (int i = 0; i < sArr.Length; i++ )
+{
+    Console.Write($"Введите элемент массива {i}: ");
+    sArr [i] = Convert.ToInt32(Console.ReadLine());
+    if (sArr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.Write("[");
+Console.Write(string.Join(", ", sArr));
+Console.WriteLine("]");
+Console.Write($"Элементов > 0 введено {count}");
