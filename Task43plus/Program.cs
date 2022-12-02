21b0dc1c483c2b4eccdae66c5d79dd3bd481ba78
MegaLitr <max.chistilin@gmail.com>
2022-12-02 17:58:26 +0300

    done Task 43plus

---
+++ Task43plus/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+// 12345 -> 5
+
+//это странно, но вначале я написал программу как она должна, по идее, работать, и толко потом понял как она действительно работает
+
+int CountDigit(int num)
+{
+    if (num/10 == 0) return 1;
+    else return CountDigit(num/10) + 1;
+}
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Цифр в числе {CountDigit(number)}");
