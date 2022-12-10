59b751167727430b408d9d1ef6bd85de478a80e7
MegaLitr <max.chistilin@gmail.com>
2022-12-10 15:07:41 +0300

    done Task 58

---
+++ Task58/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] FillMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultyMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] matrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                sum += matrix1[i, k] * matrix2[k, j];
+            }
+            matrix[i, j] = sum;
+        }
+    }
+    return matrix;
+}
+
+int m = 2; // array1 rows
+int n = 2; // array1 columns & array2 rows
+int p = 2; // array2 columns
+int[,] matrix1 = FillMatrix(m, n);
+PrintMatrix(matrix1);
+Console.WriteLine();
+int[,] matrix2 = FillMatrix(n, p);
+PrintMatrix(matrix2);
+int[,] xmatrix = MultyMatrix(matrix1, matrix2);
+Console.WriteLine();
+PrintMatrix(xmatrix);
