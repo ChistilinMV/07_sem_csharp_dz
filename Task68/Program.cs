6c0216090a0640288926f4663b1cdfd6c3555f18
MegaLitr <max.chistilin@gmail.com>
2022-12-12 22:39:01 +0300

    some add in Task 68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -2,16 +2,27 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-int Ackermann(int m, int n)
+int Ack(int m, int n)
 {
-    if (m == 0) return n + 1;
-    if (m > 0 && n == 0) return Ackermann(m - 1, 1);
-    if (m > 0 && n > 0) return Ackermann(m - 1, Ackermann(m, n - 1));
-    return 1;
+    // классический метод
+    // if (m == 0) return n + 1;
+    // if (m > 0 && n == 0) return Ack(m - 1, 1);
+    // if (m > 0 && n > 0) return Ack(m - 1, Ack(m, n - 1));
+    // return 1;
+
+    // более эффективная частично итерационная реализация
+    while (m != 0)
+    {
+        if (n == 0) n = 1;
+        else n = Ack(m, n - 1);
+        m = m - 1;
+    }
+    return n + 1;
 }
 
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Функция Аккермана для m={m}, n={n} = {Ackermann(m, n)}");
+if (m >= 0 & n > 0) Console.WriteLine($"Функция Аккермана для M={m}, N={n} = {Ack(m, n)}");
+else Console.WriteLine($"Числа должны быть неотрицателные");
